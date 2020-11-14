d3a89937e93cba7a0640345cb4f90e13bac1ddd4
Y.Koo Lee <yongku84@gmail.com>
2020-11-14 10:32:22 +0900

    Update end-point of thegraph

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -4,7 +4,7 @@ import { HttpLink } from 'apollo-link-http'
 
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: 'https://api.thegraph.com/subgraphs/name/zippoxer/sushiswap-subgraph-fork'
+    uri: 'https://api.thegraph.com/subgraphs/name/cityswap/cityswap'
   }),
   cache: new InMemoryCache(),
   shouldBatch: true
